35687e1a5bb766cb94977e966d44171eac175725
farrelanz06 <farrelanzraz@gmail.com>
2025-10-20 09:36:58 +0700

    Add try-catch and some changes

--- Codelab/src/Main.java
+++ Codelab/src/Main.java
@@ -1,15 +1,27 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
+/**
+ * This program is to determine whether the number is odd or even.
+ *
+ */
 public class Main {
     public static void main(String[] args) {
+        System.out.println("====EVEN ODD====\n");
         Scanner input = new Scanner(System.in);
-        System.out.print("Input Value: ");
-        int value = input.nextInt();
-        if( value % 2 == 0){
-            System.out.println("Even");
+        try {
+            System.out.print("Input Value: ");
+            int value = input.nextInt();
+            System.out.println("--------------");
+            if (value % 2 == 0) {
+                System.out.println("The value is Even");
+            } else {
+                System.out.println("The value is Odd");
+            }
         }
-        else{
-            System.out.println("Odd");
+        catch (InputMismatchException e) {
+            System.out.println("Invalid Input");
         }
+        input.close();
     }
 }
